2d1bf86623425b46698fa0055ef27c80bc4fd448
ryzenix <104848275@student.swin.edu.au>
2024-08-14 23:40:42 +0700

    just do it

--- handler/crypto.js
+++ handler/crypto.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 const contract = require('@truffle/contract');
 
-const provider = new Web3.providers.HttpProvider("http://47.129.33.74:8545");
+const provider = new Web3.providers.HttpProvider(process.env.GANACHE_URL);
 const TourNFTArtifact = require('../build/contracts/TourNFT.json');
 
 const web3 = new Web3(provider);
